1e384427d928d5a9097c5b3c62e21ca3011e5789
JohnK517 <jkovarik517@gmail.com>
2026-07-29 21:25:11 -0600

    Initial Daybreak Prototype
    - Completed UI layout.
    - Added Widgets panel.
    - Added Media panel.
    - Added Apps panel.
    - Added navigation.
    - Added exit overlay.

---
+++ daybreak/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace daybreak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // App Buttons
+        private void Flow_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.Navigate(new FlowPage());
+        }
+
+        private void Fuel_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.Navigate(new FuelPage());
+        }
+
+        private void Form_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.Navigate(new FormPage());
+        }
+
+        private void Feel_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.Navigate(new FeelPage());
+        }
+
+        // Exit Buttons
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            ExitOverlay.Visibility = Visibility.Visible;
+        }
+
+        private void ExitNo_Click(object sender, RoutedEventArgs e)
+        {
+            ExitOverlay.Visibility = Visibility.Collapsed;
+        }
+
+        private void ExitYes_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
